6568d5bb2eef9252c916c49e4c798a9a5ba44359
Imran <30463115+ImranR98@users.noreply.github.com>
2024-11-23 18:50:40 -0500

    Create analytics.js

---
+++ pages/assets/analytics.js
@@ -0,0 +1,5 @@
+var script = document.createElement("script");
+script.defer = true;
+script.setAttribute("data-domain", "wiki.obtainium.imranr.dev");
+script.src = "https://plausible.imranr.dev/js/script.outbound-links.js";
+document.head.appendChild(script);
